e96535c2380a0167285df640e8c9054ad4a8726b
Sam Hields <samhields@Sams-MacBook-Pro-6.local>
2018-09-30 18:16:07 +0100

    MERN front and backend implement. Currently static data.

---
+++ client/src/components/ShoppingList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import uuid from 'uuid';
+
+class ShoppingList extends Component {
+    state = {
+        items: [
+            {id: uuid(), name: 'Eggs'},
+            {id: uuid(), name: 'Milk'},
+            {id: uuid(), name: 'Sugar'},
+            {id: uuid(), name: 'Water'}
+        ]
+    }
+}
+
+export default ShoppingList;
