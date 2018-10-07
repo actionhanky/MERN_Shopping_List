12cbd9023370ae8f7f4781831dc3762bfecebd39
Sam Hields <samhields@Sams-MacBook-Pro-6.local>
2018-10-07 15:57:37 +0100

    ItemReducer being implemented

---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,8 @@
+const initialState = {
+    items: [
+        {id: uuid(), name: 'Eggs'},
+        {id: uuid(), name: 'Milk'},
+        {id: uuid(), name: 'Sugar'},
+        {id: uuid(), name: 'Water'}
+    ]
+}
