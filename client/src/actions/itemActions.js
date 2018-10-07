137ef355604a4271f95203693f973ea966305a57
Sam Hields <samhields@Sams-MacBook-Pro-6.local>
2018-10-07 18:31:24 +0100

    Redux implemented - 1 error on Expected the reducer to be a function

---
+++ client/src/actions/itemActions.js
@@ -0,0 +1,7 @@
+import { ADD_ITEM,  DELETE_ITEM, GET_ITEMS } from "./types";
+
+export const getItems = () => {
+    return {
+        type: GET_ITEMS
+    };
+};
--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,3 +1,6 @@
+import uuid from 'uuid';
+import { ADD_ITEM, DELETE_ITEM, GET_ITEMS } from "../actions/types";
+
 const initialState = {
     items: [
         {id: uuid(), name: 'Eggs'},
@@ -5,4 +8,15 @@ const initialState = {
         {id: uuid(), name: 'Sugar'},
         {id: uuid(), name: 'Water'}
     ]
+}
+
+export default function(state = initialState, action) {
+    switch(action.type) {
+        case GET_ITEMS:
+            return {
+                ...state
+            }
+        default:
+            return state;
+    }
 }
