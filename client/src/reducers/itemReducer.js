37ab7284b6c0fc911664c8c6932eea98eb87f3e4
FTruglio <frank@truglioventures.com>
2018-10-07 13:48:27 -0400

    Updated ItemReducer.js and recuders/index.js
    Added a variable reducer to itemReducers which is then
    exportable as {reducer}. Added logic to reducers/index.js
    which uses combinReducers middleware to add all reducers
    to the global state.
    
    [Ticket: X]

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,22 +1,22 @@
 import uuid from 'uuid';
-import { ADD_ITEM, DELETE_ITEM, GET_ITEMS } from "../actions/types";
+import { ADD_ITEM, DELETE_ITEM, GET_ITEMS } from '../actions/types';
 
 const initialState = {
-    items: [
-        {id: uuid(), name: 'Eggs'},
-        {id: uuid(), name: 'Milk'},
-        {id: uuid(), name: 'Sugar'},
-        {id: uuid(), name: 'Water'}
-    ]
-}
+	items: [
+		{ id: uuid(), name: 'Eggs' },
+		{ id: uuid(), name: 'Milk' },
+		{ id: uuid(), name: 'Sugar' },
+		{ id: uuid(), name: 'Water' },
+	],
+};
 
-export default function(state = initialState, action) {
-    switch(action.type) {
-        case GET_ITEMS:
-            return {
-                ...state
-            }
-        default:
-            return state;
-    }
-}
+export const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case GET_ITEMS:
+			return {
+				...state,
+			};
+		default:
+			return state;
+	}
+};
